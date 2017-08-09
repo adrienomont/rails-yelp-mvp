03ca8f875b0a61d670ca7fb38cb2b72f246a1a92
adrienomont <adriomont@gmail.com>
2017-08-09 15:12:45 +0200

    restaurants & reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+  10.times do
+    resto = Restaurant.new(
+      name: Faker::Book.title,
+      address: Faker::Address.street_address,
+      phone_number: Faker::PhoneNumber.phone_number,
+      category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+    resto.save!
+  end
+
+puts 'Finished!'
